43b7d1db3994c280796632d0a9929817397b5905
sava-1729 <alstav.trivas.sava@gmail.com>
2020-05-27 03:02:37 +0530

    updated transaction .js and .html

--- app/transaction.js
+++ app/transaction.js
@@ -2,50 +2,83 @@ var web3_lib = require('web3')
 // var tx_lib = require('ethereumjs-tx').Transaction
 var web3 = new web3_lib('HTTP://127.0.0.1:7545')
 
+var cb_function = (err, val) => { console.log("error:", err, "returned: ", val); return val }
+
 const contractAddress = ''
 const contractABI = ''
 
 const Hospital = new web3.eth.Contract(contractABI, contractAddress)
 
-var assert = require('assert');
+function sign(txData, SENDER_PRIVATE_KEY)
+{
+    const txObj = new tx_lib(txData)
+    txObj.sign(SENDER_PRIVATE_KEY)
+    const serialized = txObj.serialize().toString('hex')
+    const raw = '0x' + serialized
+    return raw
+}
+
+function getTxCount(sender_pkey)
+{
+    return web3.eth.getTransactionCount(sender_pkey, cb_function)
+}
+
+function getGasPrice()
+{
+    return web3.eth.getGasPrice(cb_function)
+}
+
+function getEthereumTransactionStruct(sender_pkey, receiver_pkey, amount, gasLimit)
+{
+    txCount = getTxCount(sender_pkey)
+    gasPrice = getGasPrice()
+    var txData = {
+        nonce : web3.utils.toHex(txCount),
+        to : web3.utils.toHex(receiver_pkey),
+        gasLimit : web3.utils.toHex(gasLimit),
+        gasPrice : web3.utils.toHex(gasPrice),
+        value : web3.utils.toHex(amount)
+    }
+    return txData
+}
+
+function getContractTransactionStruct(sender_pkey, contractAddress, functionCall, gasLimit)
+{
+    txCount = getTxCount(sender_pkey)
+    gasPrice = getGasPrice()
+    var txData = {
+        nonce : web3.utils.toHex(txCount),
+        to : web3.utils.toHex(contractAddress),
+        gasLimit : web3.utils.toHex(gasLimit),
+        gasPrice : web3.utils.toHex(gasPrice),
+        data : functionCall
+    }
+    return txData
+}
+
+function makeTransaction(sender_pkey, sender_skey, receiverAddress, data, gasLimit, mode)
+{
+    txData = null
+    if (mode == "eth")
+    {
+        txData = getEthereumTransactionStruct(sender_pkey, receiverAddress, data, gasLimit)
+    }
+    elif (mode == "fnc")
+    {
+        txData = getContractTransactionStruct(sender_pkey, receiverAddress, data, gasLimit)
+    }
+    signed_data = sign(txData, sender_skey)
+    return web3.eth.sendSignedTransaction(signed_data, cb_function)
+}
 
 function reserve_bed(form)
 {
-    const txHash = form.txhash.value
     const SENDER_PUBLIC_KEY = form.pkey.value
-    const sender_private_key = form.skey.value
+    const SENDER_PRIVATE_KEY = form.skey.value
+    const amount = web3.utils.toWei(form.amt.value, 'ether')
+    const gasLimit = 40000
+    makeTransaction(SENDER_PUBLIC_KEY, SENDER_PRIVATE_KEY, contractAddress, amount, gasLimit, 'eth')
 
-    var txObj = null
-    web3.eth.getTransaction(txHash, (err, ans) => { txObj = ans })
-    const sender = txObj.from
-    const recipient = txObj.to
-    const amount = web3.utils.fromWei(txObj.value, 'ether')
-    const hash = txObj.hash
-
-    assert(sender == SENDER_PUBLIC_KEY);
-    assert(hash == txHash);
-
-    var txCount = 0
-    var cb = (err, val) => { txCount = val }
-    web3.eth.getTransactionCount(SENDER_PUBLIC_KEY, cb)
-
-    var gasPrice = 0
-    var cb = (err, val) => { gasPrice = val }
-    web3.eth.getGasPrice(cb)
-
-    var txData = {
-        nonce : web3.utils.toHex(txCount),
-        to : web3.utils.toHex(this.contractAddress),
-        gasLimit : web3.utils.toHex(30000),
-        gasPrice : web3.utils.toHex(gasPrice),
-        data : Hospital.methods.requireBed(sender, recipient, amount, hash).encodeABI()
-    }
-
-    const txObj = new tx_lib(txData)
-    txObj.sign(sender_private_key)
-    
-    const serialized = txObj.serialize().toString('hex')
-    const raw = '0x' + serialized
-    
-    web3.eth.sendSignedTransaction(raw, (err, txHash) => {console.log('err:', err, 'txHash:', txHash)})
+    const request = Hospital.methods.requireBed(SENDER_PUBLIC_KEY).encodeABI()
+    makeTransaction(SENDER_PUBLIC_KEY, SENDER_PRIVATE_KEY, contractAddress, request, gasLimit, 'fnc')
 }
